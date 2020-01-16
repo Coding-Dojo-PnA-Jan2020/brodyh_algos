a0e2fc8d60ba74c9f08c49f4c5b2460bfc00fbc0
Brody Hoskins <brody@brody.digital>
2020-01-15 23:37:57 -0800

    Add Display assignment

---
+++ singly_linked_lists/display.js
@@ -0,0 +1,43 @@
+class ListNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+  }
+  push(val) {
+    if (this.head == null) {
+      this.head = new ListNode(val);
+      return this.head;
+    }
+    var node = this.head;
+    while (node.next !== null) {
+      node = node.next;
+    }
+    node.next = new ListNode(val);
+    return node.next;
+  }
+  display() {
+    var node = this.head;
+    var str = '';
+    while (node != null) {
+      str += node.val + ', '
+      node = node.next;
+    }
+    return str.substring(0, str.length - 2);
+  }
+}
+
+// Begin shenanigans
+
+var list = new SinglyLinkedList();
+
+for (i = 0; i <= 10; i++) {
+  list.push('Person ' + i);
+}
+
+console.log(list.display());
