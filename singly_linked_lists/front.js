145f1b5bb4de308b0f54b04cb5a94c81f977bac2
Brody Hoskins <brody@brody.digital>
2020-01-22 19:15:40 -0800

    Add Front assignment

---
+++ singly_linked_lists/front.js
@@ -0,0 +1,30 @@
+class ListNode {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+  }
+  addFront(val) {
+    var node = new Node(val);
+    node.next = this.head;
+    this.head = node;
+    return this;
+  };
+  removeFront() {
+    if (this.head != null) {
+      this.head = this.head.next;
+    };
+    return this;
+  };
+  front() {
+    if (this.head == null) {
+      return null
+    };
+    return this.head.value;
+  }
+}
